ca31ab68b94af44a826cc3b9bfe1a753688371de
anzani <mudauwavhudi13@gmail.com>
2026-05-25 22:22:03 +0200

    part 1 commit
    commiting the part 1 of the project

---
+++ part_123 - Copy/src/main/java/com/mycompany/part_123/Part_123.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.part_123;
+
+/**
+ *
+ * @author mudau
+ */
+public class Part_123 {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the system!");
+    }
+        
+        public void displayStudentDetails(String name, String studentNumber) {
+            System.out.println("Student Name: " + name);
+            System.out.println("Student Number: " + studentNumber);
+
+    }
+}
---
+++ part_123 - Copy/src/main/java/com/mycompany/part_123/infoClass.java
@@ -0,0 +1,23 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.part_123;
+
+/**
+ *
+ * @author mudau
+ */
+public class infoClass {
+    // Method 1: Display student details
+    public void displayStudentDetaild(String name, String studentNumber) {
+        System.out.println("Student name: "  + name);
+        System.out.println("Student Number: " + studentNumber);
+    }
+    
+    // Method 2: Display a welcome message
+    public void displayWelcomeMessage() {
+        System.out.println("Welcome to the student marks System");
+    }
+}
+
